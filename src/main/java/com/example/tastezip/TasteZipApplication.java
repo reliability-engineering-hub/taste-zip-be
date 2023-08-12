d449422211513219a06a1a2b99ff04ebdeaa2412
lcw729 <lcw729729@gmail.com>
2023-08-12 23:10:41 +0900

    :tada: 프로젝트 초기화

---
+++ src/main/java/com/example/tastezip/TasteZipApplication.java
@@ -0,0 +1,13 @@
+package com.example.tastezip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasteZipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TasteZipApplication.class, args);
+    }
+
+}
