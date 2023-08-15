0d0c5a682ba47a590f419a924d6faa0ce6b26055
lcw729 <lcw729729@gmail.com>
2023-08-15 17:39:22 +0900

    :recycle: 상황에 맞는 httpstatus code 반환

--- src/main/java/com/example/tastezip/api/DiaryApi.java
+++ src/main/java/com/example/tastezip/api/DiaryApi.java
@@ -7,6 +7,9 @@ import com.example.tastezip.model.Diary;
 import com.example.tastezip.service.DiaryService;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -21,43 +24,47 @@ public class DiaryApi {
     }
 
     @PostMapping("/diaries")
-    public void createDiary(
+    public ResponseEntity<Void> createDiary(
             @RequestBody CreateDiaryRequest request
     ){
         diaryService.create(request);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @GetMapping("/diaries/{diaryId}")
-    public DiaryResponse getDiary(
+    public ResponseEntity<DiaryResponse> getDiary(
             @PathVariable Long diaryId
     ){
-        return diaryService.get(diaryId);
+        return new ResponseEntity<>(diaryService.get(diaryId), HttpStatus.OK);
     }
 
+    // TODO : 커스텀 페이지네이션 요청 객체 구현하기
     @GetMapping("/diaries")
-    public PageResponse<Diary> getAllDiaries(
+    public ResponseEntity<PageResponse<Diary>> getAllDiaries(
             @RequestParam Optional<Integer> page,
             @RequestParam Optional<Integer> size
     ){
         Pageable pageable = PageRequest.of(page.orElse(0), size.orElse(10));
-        return diaryService.findAll(pageable);
+        return new ResponseEntity<>(diaryService.findAll(pageable), HttpStatus.OK);
     }
 
 
     @PutMapping("/diaries/{diaryId}")
-    public void editDiary(
+    public ResponseEntity<Void> editDiary(
             @PathVariable Long diaryId,
             @RequestBody CreateDiaryRequest request
 
     ){
         diaryService.update(diaryId, request);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @DeleteMapping("/diaries/{diaryId}")
-    public void deleteDiary(
+    public ResponseEntity<Void> deleteDiary(
             @PathVariable Long diaryId
 
     ){
         diaryService.delete(diaryId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
