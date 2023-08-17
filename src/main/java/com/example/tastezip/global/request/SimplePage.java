dbba61eae1eebfe74bba7bf32cdff864042db4ae
lcw729 <lcw729729@gmail.com>
2023-08-17 23:18:47 +0900

    :recycle: 페이지 조회 시 디폴트 값 설정

--- src/main/java/com/example/tastezip/global/request/SimplePage.java
+++ src/main/java/com/example/tastezip/global/request/SimplePage.java
@@ -7,6 +7,17 @@ public record SimplePage(
         Integer page,
         Integer size
 ) {
+
+    public SimplePage {
+        if ( page == null ) {
+            page = 0;
+        }
+
+        if ( size == null ) {
+            size = 10;
+        }
+    }
+
     public Pageable toPageable(){
         return PageRequest.of(this.page, this.size);
     }
